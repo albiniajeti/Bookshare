75784191079f206de70c727a526c78f5981a9678
AlbinAjeti <44206877+albiniajeti@users.noreply.github.com>
2018-10-31 13:00:01 +0100

    Added Login
    Added login form and connected the "Not registered yet" link with the sing up form.

---
+++ login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+   
+    <title>Book Store | </title>
+    
+     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/4-col-portfolio.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">USER LOGIN</h4>
+                </div>
+            </div>
+             
+         
+    <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h1>LOGIN FORM</h1>
+                </div>
+            <div class="panel-body">
+
+    <form role="form" method="post">
+
+        <div class="form-group">
+            <label>Email :</label>
+            <input class="form-control" type="text" name="emailid" required autocomplete="off" />
+        </div>
+
+    <div class="form-group">
+        <label>Password :</label>
+        <input class="form-control" type="password" name="password" required autocomplete="off"  />
+        <p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
+    </div>
+
+ 
+
+    <button type="submit" name="login" class="btn btn-info">LOGIN</button> | 
+         <a href="signup.php">Not Register Yet</a>
+    </form>
+
+    </div>
+        </div>
+            </div>
+                </div>  
+ 
+        </div>
+            </div>
+ 
+
+</body>
+</html>
