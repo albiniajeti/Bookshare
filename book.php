7e4d715b515faa84ca93411420dcb6914e447bb4
herbsandverbs <veranda.berisha@student.cacttus.education>
2018-12-08 11:22:41 -0800

    Added book description frontend

---
+++ book.php
@@ -0,0 +1,61 @@
+<?php include "includes/header.php"; ?>
+
+<body>
+<?php include "includes/menu.php"; ?>  
+<head>
+      <link rel="stylesheet" href="css/style_book.css">
+  
+   </head>
+<br>
+                     <h3 style="color:#274056; font-size:25px; font-family: sans-serif;">Rebirth: A Fable of Love, Forgiveness, and Following Your Heart</h3>
+                     <div class="row">
+                       <div class="col-sm-3"></div>
+                     <pre>by </pre><p style="color: green; font-family: sans-serif;">Kemal Ravikant<p>
+
+                     <div class="row">
+                        <div class="col-sm-2"></div>
+
+                        <div class="col-sm-8">
+                           <div class="row">
+                              <div class="col-sm-4 require" align=center>
+                                 <div class="image">
+                                    <img src="images/Gray_book.jpg" class="img-thumbnail" alt="Picture of a book" style="width:300px; background-color:#eff2f2;">
+                                 </div>
+                              </div>
+                              <br/>
+                              <br>
+                              <div class="col-sm-8">
+                                 <div class="row">
+                                    <div class="col-sm-4">
+                                     <a href="#" class="btn btn-default" data-toggle="modal" data-target="#saveModal"id="add_btn">Request book</a> 
+                                    </div>
+                                 </div>
+                                 <br />
+                                 <div id="border_hor" style="border: 1px solid black;"><!-- border --></div>
+                                 <div class="row">
+                                    <div class="col-sm-12">
+                                       <p class="paragraph" style="font-size:15px;margin-top: 5px;font-family: serif;">
+                                          After the death of his estranged father, Amit takes his parent's ashes to the Ganges to fulfill a deathbed promise. Instead of returning home, he wanders, his pain and grief leaving him confused about his future. Almost broke, unsure about his direction in life, and running from memories, he is led by fate to the Camino de Santiago, an ancient 550-mile pilgrimage route across northern Spain.
+                                          Amit meets a variety of travelers on his journey. Some are lost and searching for answers. Others are doing their best to leave the past behind. And there are a few who walk to celebrate life. All have stories and lessons to share.
+                                          Once a reluctant pilgrim, Amit realizes he cannot stop until he completes the journey. As a traveler tells him, "Once you start walking the Camino, the Camino becomes a part of you." With each step Amit is challenged to confront his fear of following in the footsteps of his father, the loss of a woman he may love after all, and the reality of an uncertain future. 
+                                       </p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <!-- border -->
+                           <br />
+                           <div id="border_hor" style="border: 1px solid black;"><!-- border --></div>
+
+                           <br />
+                           <br />
+                         </div>
+                     </div>
+
+                  </div>
+
+
+   <!-- Footer -->
+    <?php include "includes/footer.php"; ?>
+   </body>
+</html>
